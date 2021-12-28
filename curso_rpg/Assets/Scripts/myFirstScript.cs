b2ec7825deb7be510b8849e7faeb32147ba0e463
Layton4 <Javierete4774@gmail.com>
2021-12-29 00:55:49 +0100

    Variables y métodos revisados

--- curso_rpg/Assets/Scripts/myFirstScript.cs
+++ curso_rpg/Assets/Scripts/myFirstScript.cs
@@ -7,6 +7,13 @@ public class myFirstScript : MonoBehaviour
     //public int carDoors = 4;
     public int x = 2;
     public int y = 3;
+
+
+    public int hour = 23;
+    public float pi = 3.14f;
+    public string game = "Objetivo cumplido";
+    public bool spiderman = true;
+
     void Start()
     {
         //Debug.Log(3 + 5);
